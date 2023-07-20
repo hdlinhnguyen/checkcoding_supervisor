180c7c83ba3cdf75401e732696be59718eb3bb40
Linh <linh.nguyen@ens-paris-saclay.fr>
2023-07-20 16:06:26 +0200

    Update testtotalpayoff.jl

--- testtotalpayoff.jl
+++ testtotalpayoff.jl
@@ -1,7 +1,16 @@
 ########################
-####### TOTAL PAYOFF: one of thousand tryings 
+####### Problem: conditions represent the value computed from the parameters and value of variable x or y
 #####################
 using Plots
+g = 0.5
+d = 0.25
+c = 0.2
+ϕ = 0.4
+α = 0.25
+s = 0.3 
+
+x_vals = range(0, stop = 1, length = 100)
+y_vals = range(0, stop = 1, length = 100)
 
 function W(x, y, g, d, ϕ, c, α, s)
     condition_3 = (1/d)*(1 - s - c) - (1/d)*(1 + g - d - ϕ)*x - (g/d)*x^2
@@ -40,16 +49,6 @@ function W(x, y, g, d, ϕ, c, α, s)
         return (1 - P_1 - c + ϕ*x + (α/2)*x) + s
     end
 end
-
-g = 0.5
-d = 0.25
-c = 0.2
-ϕ = 0.4
-α = 0.25
-s = 0.3 #subsidy or π^NCH
-
-x_vals = range(0, stop = 1, length = 100)
-y_vals = range(0, stop = 1, length = 100)
 
 z = [W(x, y, g, d, ϕ, c, α, s) for x in x_vals, y in y_vals]
 
