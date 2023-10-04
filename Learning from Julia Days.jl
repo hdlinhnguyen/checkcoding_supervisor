3919c00a9a6e9b42dbff1614807dfee3e4550f5c
Linh <linh.nguyen@ens-paris-saclay.fr>
2023-10-04 11:09:07 +0200

    Create Learning from Julia Days.jl

---
+++ Learning from Julia Days.jl
@@ -0,0 +1,6 @@
+#the matrix comes from here 
+
+#From Julia Day Conference to learn this 
+contour(x_vals, y_vals, z, xlabel = "P1", ylabel = "P2", zlabel = "", title = "Total Payoff")
+
+#try to fill out the region within the contour
